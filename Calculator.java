7f0f578ebd24c2b6ec39da2ea9354262a67700ac
Kartik Katiyar <kartik.katiyar@ibm.com>
2024-04-14 22:51:26 +0530

    Java calculator project initialized

---
+++ Calculator.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc= new Scanner(System.in);
+	    System.out.println("Enter the choice \n 1.addition \n 2.subtract \n 3.multiply \n 4.division");
+	    int choice =  sc.nextInt();
+		System.out.println("Enter the 1st number");
+		int a= sc.nextInt();
+		System.out.println("Enter the 2nd number");
+	    int b= sc.nextInt();
+		switch(choice)
+    {
+    case 1 :
+     
+     int add= a+b;
+     System.out.println(add);
+     
+     break; 
+  
+    case 2 :
+        int sub=a+b;
+        System.out.println(sub);
+   
+     break; 
+    
+     default : 
+     System.out.println("Invalid Choice");
+}
+		
+	}
+}
